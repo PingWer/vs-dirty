34d67fd508495378e16a8e7ece741e417d3a5b22
Mhanz3500 <127054994+Mhanz3500@users.noreply.github.com>
2025-08-28 18:23:28 +0200

    fine v1

--- dirtyfixer/dirtyfunc.py
+++ dirtyfixer/dirtyfunc.py
@@ -3,53 +3,61 @@ from vstools import depth, scale_value, ColorRange
 
 def dirty_fix(
     clip: vs.VideoNode,
-    columns: dict[int, int] = None,
-    rows: dict[int, int] = None,
-    prot_val: list[int] = [16, 235],
+    rows: dict[int, int] = {},
+    columns: dict[int, int] = {},
     video_range: list[int] = [16, 235],
-    min_val: int = 16,
-    max_val: int = 235
+    var: int = 1,
+    verbose: bool = False
 ) -> vs.VideoNode:
     """
-    Fix for dirty lines by Man3500
-    :param clip: Clip to process (YUV/GRAY 32bit, if not will be internally converted in 32bit with void dither).
-    """
-    core = vs.core
+    Fix for dirty lines by Man3500, This script amplifies by a percentage the value of the column/row.
 
-    if clip.format.color_family not in {vs.YUV, vs.GRAY}:
-        raise ValueError('dirty_fix: only YUV and GRAY formats are supported')
+    :param clip: Clip to process (GRAY 32bit, if not will be internally converted in 32bit with void dither).
+    :param rows: Receives (row_number, row_value), negative row values will darken the selected row.
+    :param columns: Receives (column_number, column_value), negative column values will darken the selected column.
+    :param video_range: This will clip the output values to the selected range.
+    :param var: Function in beta, sane values: 0.5-10.
+    :param verbose: For debugging.
+    """
+
+    if clip.format.color_family not in {vs.GRAY}:
+        raise ValueError('dirty_fix: only GRAY formats are supported')
 
     if clip.format.bits_per_sample != 32:
         clip = depth(clip, 32)
 
-    min_range = 0.1
-    max_range = 0.9
-    range_used = max_range - min_range
-    orig_transfer = clip.get_frame(0).props.get("_Transfer")
+    min_range = scale_value(video_range[0], 8, 32, ColorRange.FULL, ColorRange.FULL)
+    max_range = scale_value(video_range[1], 8, 32, ColorRange.FULL, ColorRange.FULL)
 
-    clip = clip.resize.Point(transfer_in=orig_transfer, transfer=8)  # Original Transfer to Linear
+    expr = ""
 
-    clip.set_output(1)
-
-    if rows is not None:
-        row_expr = ""
+    if rows:
         for row_num, row_val in rows:
-            fun_pos = f"{max_range} {row_val} {range_used} 100 / * - {min_range} -"
-            # fun_pos ="0.9"
-            rekt_positive = f"{fun_pos} <= 0 x {min_range} - 0.01 / x {min_range} - {fun_pos} / {range_used} * {min_range} + ?"
-            # rekt_positive = "0.1"
-            rowstr = f"Y {row_num} = {rekt_positive} "
-            row_expr = row_expr + rowstr
+            row_val = row_val/100
+            row_strength = f"1 {row_val} x 1 + x {var} + / * +"
+            rowstr = f"Y {row_num} = x {row_strength} * {min_range} {max_range} clip "
+            expr = expr + rowstr
+            if verbose:
+                print(f"row_number {row_num} has value {row_val}")
+    
+    if columns:
+        for col_num, col_val in columns:
+            col_val = col_val/100
+            col_strength = f"1 {col_val} x 1 + x {var} + / * +"
+            colstr = f"X {col_num} = x {col_strength} * {min_range} {max_range} clip "
+            expr = expr + colstr
+            if verbose:
+                print(f"column_number {col_num} has value {col_val}")
+    
+    expr = expr + "x "
 
-        row_expr = row_expr + "x "
-        for _ in range(len(rows)):
-            row_expr = row_expr + "? "
-    clip = clip.akarin.Expr(row_expr)
+    for _ in range(len(rows)):
+        expr = expr + "? "
+    for _ in range(len(columns)):
+        expr = expr + "? "
 
-    print(row_expr)
-
-    clip.set_output(2)
-
-    clip = clip.resize.Point(transfer_in=8, transfer=orig_transfer) # Linear to Original Transfer
+    if verbose:
+        print("final expr: " + expr)
+    clip = clip.akarin.Expr(expr)
 
     return clip
