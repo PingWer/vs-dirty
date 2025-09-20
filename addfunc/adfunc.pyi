64089a8a9c12bb297f540288cf4fef44376795a4
PingWer <r74michele@gmail.com>
2025-09-20 11:49:58 +0200

    fixato il passaggio degli argomenti e il tipo di ritorno
    il tipo di ritorno è cmq è buggato da me

--- addfunc/adfunc.pyi
+++ addfunc/adfunc.pyi
@@ -3,13 +3,83 @@ import vapoursynth as vs #non serve ma mi secca vedere robe rosse
 class adenoise:
     @staticmethod
     def __init__(): ...
+    
     @staticmethod
-    def scan65mm(clip:vs.VideoNode, **kwargs): ...
+    def scan65mm(clip:vs.VideoNode, 
+    thsad: int = 800,
+    tr: int = 2,
+    tr2: int = 1,
+    sigma: float = 12,
+    luma_mask_weaken: float = 0.75,
+    luma_mask_thr: float = 50,
+    chroma_strength: float = 1.0,
+    precision: bool = True,
+    chroma_masking: bool = False,
+    show_mask: int = 0,
+    flat_penalty: float = 0.5,
+    texture_penalty: float = 1.1,
+    )->vs.VideoNode: ...
+
     @staticmethod
-    def scan35mm(clip:vs.VideoNode, **kwargs): ...
+    def scan35mm(clip:vs.VideoNode, 
+    thsad: int = 800,
+    tr: int = 2,
+    tr2: int = 1,
+    sigma: float = 12,
+    luma_mask_weaken: float = 0.75,
+    luma_mask_thr: float = 50,
+    chroma_strength: float = 1.0,
+    precision: bool = True,
+    chroma_masking: bool = False,
+    show_mask: int = 0,
+    flat_penalty: float = 0.5,
+    texture_penalty: float = 1.1,
+    )->vs.VideoNode: ...
+
     @staticmethod
-    def scan16mm(clip:vs.VideoNode, **kwargs): ...
+    def scan16mm(clip:vs.VideoNode, 
+    thsad: int = 800,
+    tr: int = 2,
+    tr2: int = 1,
+    sigma: float = 12,
+    luma_mask_weaken: float = 0.75,
+    luma_mask_thr: float = 50,
+    chroma_strength: float = 1.0,
+    precision: bool = True,
+    chroma_masking: bool = False,
+    show_mask: int = 0,
+    flat_penalty: float = 0.5,
+    texture_penalty: float = 1.1,
+    )->vs.VideoNode: ...
+
     @staticmethod
-    def scan8mm(clip:vs.VideoNode, **kwargs): ...
+    def scan8mm(clip:vs.VideoNode, 
+    thsad: int = 800,
+    tr: int = 2,
+    tr2: int = 1,
+    sigma: float = 12,
+    luma_mask_weaken: float = 0.75,
+    luma_mask_thr: float = 50,
+    chroma_strength: float = 1.0,
+    precision: bool = True,
+    chroma_masking: bool = False,
+    show_mask: int = 0,
+    flat_penalty: float = 0.5,
+    texture_penalty: float = 1.1,
+    )->vs.VideoNode: ...
+
     @staticmethod
-    def digital(clip:vs.VideoNode, **kwargs): ...
+    def digital(clip:vs.VideoNode, 
+    thsad: int = 800,
+    tr: int = 2,
+    tr2: int = 1,
+    sigma: float = 12,
+    luma_mask_weaken: float = 0.75,
+    luma_mask_thr: float = 50,
+    chroma_strength: float = 1.0,
+    precision: bool = True,
+    chroma_masking: bool = False,
+    show_mask: int = 0,
+    flat_penalty: float = 0.5,
+    texture_penalty: float = 1.1,
+    )->vs.VideoNode: ...
